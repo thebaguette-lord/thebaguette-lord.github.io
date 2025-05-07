6fef052b4336e7e2607ed0a00099b157f6aad6fd
thebaguette-lord <MEY0003@tc.vic.edu.au>
2025-05-07 13:20:47 +1000

    changed input method & added checks
    buttons to onchange(), date to text for 2nd input, added checks to validate date input

--- dateToRGB.js
+++ dateToRGB.js
@@ -1,11 +1,18 @@
 function ToRGB() {
     var date = document.getElementById("dateinput").value;
     date = date.split("-")
-    var day = date[2]
+    var day = date[0]
     var month = date[1]
-    var year = date[0]
-    day = day/31*255
-    month = month/12*255
-    year = year%255
-    document.getElementById("yn").style.backgroundColor = "rgb(" + day + "," + month + "," + year +")";
+    var year = date[2]
+    if (day >= 1 & day <= 31 & month >= 1 & month <= 12 & year >= 1900 & year <= 2025)
+    {
+        day = day/31*255
+        month = month/12*255
+        year = year%255
+        document.getElementById("yn").style.backgroundColor = "rgb(" + day + "," + month + "," + year +")";
+        document.getElementById("dateinput").style.backgroundColor = "white"
+    }
+    else {
+        document.getElementById("dateinput").style.backgroundColor = "red"
+    }
 }
