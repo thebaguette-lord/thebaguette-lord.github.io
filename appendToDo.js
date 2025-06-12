a952b492fd3e12f34706cef252350118c12583ba
thebaguette-lord <MEY0003@tc.vic.edu.au>
2025-06-12 11:45:31 +1000

    cookies

--- appendToDo.js
+++ appendToDo.js
@@ -1,12 +1,14 @@
-const tasks = [];
+let tasks = [];
 var newtask = "getOutOfMyCode"
 function appendToDo() {
     newtask = document.getElementById("input").value;
     tasks.push(newtask);
+    SaveCookies();
     FixList();
 }
 function severToDo(remove) {
     tasks.splice(remove,1)
+    SaveCookies();
     FixList();
 }
 function FixList() {
@@ -20,7 +22,26 @@ function FixList() {
             const delval = i;
             li.appendChild(deleteBtn);
             deleteBtn.innerText = 'Delete';
-            deleteBtn.addEventListener('click', () => {severToDo(delval)});
-            
+            deleteBtn.addEventListener('click', () => {severToDo(delval)});        
     }
+}
+function SaveCookies() {
+    var taskStr = "";
+    for (i = 0; i < tasks.length; ++i) {
+        taskStr += tasks[i] + ",";
+    }
+    document.cookie = "tasks=" + taskStr;
+    
+}
+function GetCookies() {
+    let myCookies = document.cookie.split(";");
+    let findCookie = "tasks=";
+
+    for(i = 0; i < myCookies.length; i++){
+        if(myCookies[i].indexOf(findCookie) !== -1){
+            if(myCookies[i].substring(findCookie.length, myCookies[i].length-1) !== "=")
+            tasks = myCookies[i].substring(findCookie.length, myCookies[i].length-1).split(",");
+        }
+    }
+    FixList()
 }
